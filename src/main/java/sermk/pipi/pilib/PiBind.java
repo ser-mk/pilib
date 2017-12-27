857ed261746db8a44a85a995ee6a1e1de44882f1
Evgeny Chormonov <en.chormonov@gmail.com>
2017-12-27 20:26:12 +0300

    FEAT: add statment bind

--- src/main/java/sermk/pipi/pilib/PiBind.java
+++ src/main/java/sermk/pipi/pilib/PiBind.java
@@ -5,10 +5,8 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.os.Handler;
 import android.os.IBinder;
-import android.os.Message;
-import android.os.Messenger;
+import android.os.RemoteException;
 import android.util.Log;
 
 /**
@@ -18,18 +16,13 @@ import android.util.Log;
 public class PiBind implements ServiceConnection {
 
     private final String TAG = this.getClass().getName();
-/*
-    private Messenger incomingMessanger = new Messenger(new IncomingHandler());
-    private Messenger bindMessanger = new Messenger(new DefaultHandler());
-    */
-    private boolean isBound = false;
+
+    private enum BindState { UNDEFINED, BINDED, UNBINED, REMOTE_EXEPTION }
+    private BindState state = BindState.UNDEFINED;
     private Pinterface mIRemoteService;
 
-    private static String packageName =  "sermk.pipi.game";
-    private static String className =  "sermk.pipi.game.PIService";
-
-
-
+    private static String packageName =  "sermk.pipi.pilauncher";
+    private static String className =  "sermk.pipi.pilauncher.PIService";
 
     public PiBind(Activity act) {
         Log.v(TAG,"start bind");
@@ -39,21 +32,44 @@ public class PiBind implements ServiceConnection {
         Log.v(TAG, "bind to service " + packageName + " " + className + " is " + ret);
     }
 
+    public static final int CONNECTED_PROBLEM = -1;
+    public static final int POSITION_UNDEFINED = -2;
+
+    public static final int POSITION_MIN = 0;
+    public static final int POSITION_MAX = 255;
+
+    public int getPosition(){
+        if(state == BindState.BINDED && mIRemoteService != null){
+            try {
+                return mIRemoteService.getPosition();
+            } catch (RemoteException e) {
+                e.printStackTrace();
+                state = BindState.REMOTE_EXEPTION;
+            }
+        }
+        if(state == BindState.UNDEFINED){
+            return POSITION_UNDEFINED;
+        }
+
+        return CONNECTED_PROBLEM;
+    }
+
     @Override
     public void onServiceConnected(ComponentName name, IBinder service) {
         Log.v(TAG,"onServiceConnected " + service.toString());
         mIRemoteService = Pinterface.Stub.asInterface(service);
-        isBound = true;
+        state = BindState.BINDED;
     }
 
     @Override
     public void onServiceDisconnected(ComponentName name) {
         Log.v(TAG,"onServiceDisconnected");
         mIRemoteService = null;
-        isBound = false;
+        state = BindState.UNBINED;
     }
 
     public void release(Activity act){
+        Log.v(TAG, "unbind " + packageName + " " + className + " service");
         act.unbindService(this);
     }
 /*
