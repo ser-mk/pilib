0efc114b7ec271f112e011659ab3bce605ab37c3
Evgeny Chormonov <en.chormonov@gmail.com>
2018-01-10 18:22:46 +0300

    FEAT: add command collection class

---
+++ src/main/java/sermk/pipi/pilib/CommandCollection.java
@@ -0,0 +1,10 @@
+package sermk.pipi.pilib;
+
+/**
+ * Created by echormonov on 10.01.18.
+ */
+
+public class CommandCollection {
+    public static final String ACTION_RECIVER_SET_SETTINGS = "PI.SETTINGS.SET";
+    public static final String ACTION_RECIVER_SAVE_SETTINGS = "PI.SETTINGS.SAVE";
+}
