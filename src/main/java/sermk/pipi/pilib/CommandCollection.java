e4bb51d47ab6ea55d29efcfae62732e29f9f2e51
Evgeny Chormonov <en.chormonov@gmail.com>
2018-03-19 02:23:22 +0300

    FEAT: add piball actions settings

--- src/main/java/sermk/pipi/pilib/CommandCollection.java
+++ src/main/java/sermk/pipi/pilib/CommandCollection.java
@@ -13,6 +13,9 @@ public class CommandCollection {
     public static final String ACTION_RECIVER_REMOVE_PACKAGE = "REMOVE_PACKAGE";
     public static final String ACTION_RECIVER_SET_PI_STATUS ="SET_PI_STATUS";
 
+    public static final String ACTION_RECIVER_PIBALL_GET_SETTINGS ="PIBALL_GET_SETTINGS";
+    public static final String ACTION_RECIVER_PIBALL_SET_SETTINGS ="PIBALL_SET_SETTINGS";
+
     public static final String FIELD_RECIVER_DATA_TEXT = Intent.EXTRA_TEXT;
     public static final String FIELD_RECIVER_ATTACHED_BYTES = Intent.EXTRA_INITIAL_INTENTS;
 }
